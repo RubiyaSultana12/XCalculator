73977742c5656e06897e153008ffb65f7f712dd6
RubiyaSultana12 <rubiya.sultana22@gmail.com>
2024-12-26 20:26:02 +0530

    test commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import "./style.css";
+import React, { useState } from "react";
+import { evaluate } from "mathjs";
+
+
+function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleClick = (value) => {
+    setInput((prevInput) => prevInput + value);
+  };
+
+  const handleCalculator = () => {
+    if (input.trim === "") {
+      setResult("incomplete expression");
+    } else {
+      try {
+        setResult(evaluate(input));
+      } catch (error) {
+        setResult("error");
+      }
+    }
+  };
+
+  const handleClear = () => {
+    setInput("");
+    setResult("");
+  };
+  return (
+    <div className="calculator">
+      <h1>React Calculator</h1>
+      <div className="inputbox">
+        <input type="text" value={input} readOnly/>
+        <div className="result">{result}</div>
+      </div>
+      <div className="buttons">
+        {[
+          "7",
+          "8",
+          "9",
+          "+",
+          "4",
+          "5",
+          "6",
+          "-",
+          "1",
+          "2",
+          "3",
+          "*",
+          "C",
+          "0",
+          "=",
+          "/",
+        ].map((item, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              if (item === "=") {
+                handleCalculator();
+              } else if (item === "C") {
+                handleClear();
+              } else {
+                handleClick(item);
+              }
+            }}
+          >
+            {item}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
